453030feda2a42b4f1161b8c51bb7eefddb7a3f7
Haolin Song <72663301+ethanshl@users.noreply.github.com>
2022-12-10 20:13:14 -0500

    Login and registration functions done

--- app/src/main/java/com/example/a6216groupproject/SignupActivity.java
+++ app/src/main/java/com/example/a6216groupproject/SignupActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.text.Editable; // for EditText event handling
-import android.text.TextWatcher; // EditText listener
-import android.widget.CompoundButton;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.view.View.OnFocusChangeListener;
+import android.text.TextUtils;
 
 
 
@@ -31,21 +28,23 @@ public class SignupActivity extends Activity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usernameString = (String) username.getText().toString();
-                String passwordString = (String) password.getText().toString();
-                String passwordConfirmString = (String) passwordConfirm.getText().toString();
+                String usernameString = username.getText().toString();
+                String passwordString = password.getText().toString();
+                String passwordConfirmString = passwordConfirm.getText().toString();
 
-                if (usernameString == "" || passwordString == "" || passwordConfirmString == ""){
+                if (TextUtils.isEmpty(usernameString) || TextUtils.isEmpty(passwordString)|| TextUtils.isEmpty(passwordConfirmString)){
                     Toast.makeText(getApplicationContext(), "Please fill in your account information.", Toast.LENGTH_LONG).show();
-                } else if (passwordString != passwordConfirmString){
+                } else if (! passwordString.equals(passwordConfirmString)){
                     Toast.makeText(getApplicationContext(), "Passwords must match!", Toast.LENGTH_LONG).show();
                 } else {
                     usernameAcc =  usernameString;
                     passwordAcc = passwordString;
                     Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    intent.putExtra("usernameStore", usernameString);
+                    intent.putExtra("passwordStore", passwordString);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "Please fill in your account information.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Your Account has been created successfully!", Toast.LENGTH_LONG).show();
                 }
 
 
