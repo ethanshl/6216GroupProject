d018a14175400522acc9846041133f008419fa09
Haolin Song <72663301+ethanshl@users.noreply.github.com>
2022-12-11 19:17:40 -0500

    1

--- app/src/main/java/com/example/a6216groupproject/PostActivity.java
+++ app/src/main/java/com/example/a6216groupproject/PostActivity.java
@@ -1,27 +1,31 @@
 package com.example.a6216groupproject;
 
 import android.app.Activity;
+import android.os.Bundle; // for saving state information
+import android.text.TextUtils;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText; // for bill amount input
+import android.widget.Toast;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends Activity {
-
-
+public class PostActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        //Button loginButton = (Button) findViewById(R.id.postButton);
+        setContentView(R.layout.post);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
-        // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.home);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 switch(item.getItemId())
                 {
                     case R.id.message:
@@ -29,6 +33,8 @@ public class HomeActivity extends Activity {
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.create:
                         startActivity(new Intent(getApplicationContext(),CreateActivity.class));
@@ -39,9 +45,5 @@ public class HomeActivity extends Activity {
             }
         });
     }
-    public void postLayout(View view){
-        Intent intent = new Intent(HomeActivity.this, PostActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent);
-    }
 }
+
